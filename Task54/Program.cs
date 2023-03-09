c5a8b2a083733cf667796fa0a57419fe65d0e9a5
Tatiana <Rybalkinatr@gmail.com>
2023-03-09 15:25:25 +0400

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine()!);
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine()!);
+
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+SortArray(array);
+WriteLine();
+PrintArray(array);
+
+
+// Метод заполнения массива рандомно числами от 1 до 9
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+// Метод сортировки элементов в строке двумерного массива, по убыванию
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int z = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = z;
+                }
+            }
+        }
+    }
+}
+
+// Метод вывода двумерного массива
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]} ");
+        }
+        WriteLine();
+    }
+}
