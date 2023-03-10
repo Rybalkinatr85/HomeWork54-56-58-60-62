ae33b6b34e78cc383179da8c093b1e0755aeefed
Tatiana <Rybalkinatr@gmail.com>
2023-03-10 10:42:04 +0400

    Task58.1

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -11,63 +11,39 @@ using static System.Console;
 
 Clear();
 
-// WriteLine("Введите размеры матриц и диапазон случайных значений:");
-// int m = InputNumbers("Введите число строк 1-й матрицы: ");
-// int n = InputNumbers("Введите число столбцов 1-й матрицы (и строк 2-й): ");
-// int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
-
 Write("Введите количество строк первой матрицы: ");
-int rows1 = int.Parse(ReadLine()!);
+int rows1 = int.Parse(Console.ReadLine()!);
 Write("Введите количество столбцов первой матрицы: ");
-int columns1 = int.Parse(ReadLine()!);
+int columns1 = int.Parse(Console.ReadLine()!);
 
 Write("Введите количество строк второй матрицы: ");
-int rows2 = int.Parse(ReadLine()!);
+int rows2 = int.Parse(Console.ReadLine()!);
 Write("Введите количество столбцов второй матрицы: ");
-int columns2 = int.Parse(ReadLine()!);
+int columns2 = int.Parse(Console.ReadLine()!);
 
-
-int[,] firstMartrix = GetArray[rows1, columns1, 0, 10];
-
-WriteLine($"Первая матрица:");
-PrintArray(firstMartrix);
-
-int[,] secomdMartrix = GetArray[rows2, columns2, 0, 10];
-
-WriteLine($"Вторая матрица:");
-PrintArray(secomdMartrix);
-
-int[,] resultMatrix = GetArray[rows1, columns2];
-
-MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
-WriteLine($"Произведение первой и второй матриц:");
-PrintArray(resultMatrix);
-
-void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+int[,] MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix)
 {
-    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    int[,] rezultMatrix = new int [firstMartrix.GetLength(0), secomdMartrix.GetLength(1)];
+    for (int i = 0; i < firstMartrix.GetLength(0); i++)
     {
-        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        for (int j = 0; j < secomdMartrix.GetLength(1); j++)
         {
-            int sum = 0;
-            for (int k = 0; k < firstMartrix.GetLength(1); k++)
-            {
-                sum += firstMartrix[i, k] * secomdMartrix[k, j];
-            }
-            resultMatrix[i, j] = sum;
+            
+            rezultMatrix[i, j] = firstMartrix[i,j] * secomdMartrix[i,j] ;
         }
     }
+    return rezultMatrix;
 }
 
-// Метод заполнения массива рандомно числами от 1 до 9
-int[,] GetArray(int m, int n, int min, int max)
+// Метод заполнения массива рандомно числами от 1 до 10
+int[,] GetArray(int m, int n)
 {
     int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(min, max + 1);
+            result[i, j] = new Random().Next(0, 11);;
         }
     }
     return result;
@@ -85,3 +61,14 @@ void PrintArray(int[,] array)
         WriteLine();
     }
 }
+int[,] firstMartrix = GetArray(rows1, columns1);
+WriteLine($"Первая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = GetArray(rows2, columns2);
+WriteLine($"Вторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] result = MultiplyMatrix(firstMartrix, secomdMartrix);
+WriteLine($"Произведение первой и второй матриц:");
+PrintArray(result);
